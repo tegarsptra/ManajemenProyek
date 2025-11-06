3f1e3335ddfce374643cd70a59d144dd9e501afe
DanielRio <23n10003@student.unika.ac.id>
2025-11-06 22:29:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const wisataData = [
     deskripsi:
       "Bangunan bersejarah peninggalan Belanda yang kini menjadi ikon Kota Semarang.",
     gambar: "images/lawangsewu.jpg",
-    peta: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.738918186609!2d110.410543!3d-6.982986!2m3!1f0!2f0!3f0!"
+    peta: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.738918186609!2d110.410543!3d-6.982986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708cfeaa5e5f15%3A0x8ef91f6438c2a249!2sLawang%20Sewu!5e0!3m2!1sid!2sid!4v1709373361043!5m2!1sid!2sid"
   },
   {
     id: 2,
@@ -16,7 +16,7 @@ const wisataData = [
     deskripsi:
       "Kawasan wisata bersejarah dengan bangunan klasik bergaya Eropa yang masih terjaga.",
     gambar: "images/kotalama.jpg",
-    peta: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.809708951011!2d110.426693!3d-6.978124"
+    peta: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.809708951011!2d110.426693!3d-6.978124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b29ddf4a2e1%3A0x4f3a6b16c91d4dd9!2sKota%20Lama%20Semarang!5e0!3m2!1sid!2sid!4v1709373376001!5m2!1sid!2sid"
   },
   {
     id: 3,
@@ -25,7 +25,7 @@ const wisataData = [
     deskripsi:
       "Klenteng bersejarah yang menjadi simbol toleransi dan wisata budaya di Semarang.",
     gambar: "images/sampookong.jpg",
-    peta: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.753082274225!2d110.392886!3d-6.981928"
+    peta: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.753082274225!2d110.392886!3d-6.981928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708a2a1e6caa9f%3A0x2e96ef7a64bdb62!2sSam%20Poo%20Kong!5e0!3m2!1sid!2sid!4v1709373389582!5m2!1sid!2sid"
   },
   {
     id: 4,
@@ -34,7 +34,7 @@ const wisataData = [
     deskripsi:
       "Masjid megah dengan menara tinggi dan pemandangan kota yang indah.",
     gambar: "images/majt.jpg",
-    peta: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.870208299078!2d110.444858!3d-6.972258"
+    peta: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.870208299078!2d110.444858!3d-6.972258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708bc075b12c5b%3A0x25b122dc4c8e5c16!2sMasjid%20Agung%20Jawa%20Tengah!5e0!3m2!1sid!2sid!4v1709373399400!5m2!1sid!2sid"
   },
   {
     id: 5,
@@ -43,7 +43,7 @@ const wisataData = [
     deskripsi:
       "Tebing bekas tambang yang kini jadi spot foto alam eksotis di Semarang.",
     gambar: "images/browncanyon.jpg",
-    peta: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.754522240178!2d110.482482!3d-7.024304"
+    peta: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.754522240178!2d110.482482!3d-7.024304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708ef0c7b96be1%3A0x4bcd314167ccf88c!2sBrown%20Canyon!5e0!3m2!1sid!2sid!4v1709373409281!5m2!1sid!2sid"
   },
 ];
 
@@ -109,5 +109,7 @@ if (detailContainer) {
       <p>${wisata.deskripsi}</p>
       <iframe src="${wisata.peta}" width="100%" height="350" style="border:0;" allowfullscreen></iframe>
     `;
+  } else {
+    detailContainer.innerHTML = "<p>Data wisata tidak ditemukan 😢</p>";
   }
 }
